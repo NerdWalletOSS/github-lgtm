ad6d1cfc7796c4d6e5e88c14d8eafcc6ee5a79a0
Chase Seibert <chase.seibert+github@gmail.com>
2016-08-12 13:13:20 -0700

    Fix tests, add OWNERS

--- lgtm/integrations/base.py
+++ lgtm/integrations/base.py
@@ -1,9 +1,10 @@
-def get_options_defaults_dict(integration_type):
+def get_options_defaults_dict(integration_type, env=None):
+    env = env or {}
     if integration_type == 'jenkins':
         import jenkins
-        return jenkins.get_pull_request_dict()
+        return jenkins.get_pull_request_dict(env)
     elif integration_type == 'travis':
         import travis
-        return travis.get_pull_request_dict()
+        return travis.get_pull_request_dict(env)
     else:
         raise NotImplementedError()
